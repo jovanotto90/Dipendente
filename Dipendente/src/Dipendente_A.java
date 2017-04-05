bdccf06b3056db755e1b2243701354f3a9dc909c
MarcoFiorillo <MarcoFiorillo@users.noreply.github.com>
2017-04-05 15:17:51 +0200

    Update Dipendente_A.java

--- Dipendente/src/Dipendente_A.java
+++ Dipendente/src/Dipendente_A.java
@@ -1,31 +1,54 @@
 
-public class Dipendente_A extends Dipendente {
-	
+public class DipA extends Dipendente {
+
 	private int malattia;
-	
-	public Dipendente_A (String matricola, double stipendio, double straordinario) {
-		super (matricola, stipendio, straordinario);
-		this.malattia =  0;
+
+	public DipA(String matricola, float stipendio, float straordinario) {
+		super(matricola, stipendio, straordinario);
+		malattia = 0;
 	}
-	
-	public void prendiMalattia (int giorni){
-		this.malattia += giorni;
+
+	public DipA(Dipendente d) {
+		super(d.getMatricola(), d.getStipendio(), d.getStraordinario());
+		malattia = 0;
 	}
-	
-	public int getMalattia (){
-		return this.malattia;
+
+	public int getMalattia() {
+		return malattia;
 	}
-	
-	public double paga (int ore){
-		double p = super.paga(ore);
-		if (this.malattia == 0)
-			return p;
-		else
-			return p - (this.malattia * 15.0);
+
+	public void setMalattia(int malattia) {
+		this.malattia = malattia;
 	}
-	
-	public void stampaMalattia (){
-		System.out.println ("Malattia: " + this.malattia);
+
+	public void malattia(int n) {
+		malattia += n > 0 ? n : 0;
+	}
+
+	@Override
+	public float paga(int ore) {
+		float tmp = super.paga(ore);
+		return malattia == 0 ? tmp : tmp - (15 * getMalattia());
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "DipA [malattia=" + malattia + "]";
+	}
+
+	public void stampaMalattia() {
+		System.out.println(this);
+	}
+
+	public static void main(String[] args) {
+		Dipendente d = new Dipendente("123", 1000, 20);
+		d.stampa();
+		System.out.println("paga per 3 ore straordinario:" + d.paga(3));
+		DipA d2 = new DipA(d);
+		d2.stampaMalattia();
+		d2.malattia(10);
+		System.out.println("paga per 3 ore straordinario e 10g malattia:" + d2.paga(3));
+		d2.stampaMalattia();
 	}
 
 }
